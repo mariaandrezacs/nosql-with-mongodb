a250e5ed8f0c69eaf3ca0e0e151b618f3931b533
Maria Andreza C Santos <mandreza008@gmail.com>
2025-04-22 23:05:20 -0300

    feat: implementa validator update

---
+++ src/validators/registry_update.py
@@ -0,0 +1,21 @@
+from cerberus import Validator
+
+from src.errors.types.http_unprocessable_entity import \
+    HttpUnprocessableEntityError
+
+
+def registry_update_validator(body: any):
+    body_validator = Validator({
+        "data": {
+            "type": "dict",
+            "schema": {
+                "name": {"type": "string"},
+                "address": {"type": "string"},
+                "cupom": {"type": "boolean"},
+            }
+        }
+    })
+
+    response = body_validator.validate(body)
+    if response is False:
+        raise HttpUnprocessableEntityError(body_validator.errors)
