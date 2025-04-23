c32e5df8be1a3980ea4cbfd4952b4245802ad420
Maria Andreza C Santos <mandreza008@gmail.com>
2025-04-22 23:03:52 -0300

    feat: atualiza a interface do repository

--- src/models/repository/interfaces/orders_repository.py
+++ src/models/repository/interfaces/orders_repository.py
@@ -3,38 +3,50 @@ from abc import ABC, abstractmethod
 
 class OrdersRepositoryInterface(ABC):
     @abstractmethod
-    def insert_document(self, document: dict) -> None: pass
+    def insert_document(self, document: dict) -> None:
+        pass
 
     @abstractmethod
-    def insert_list_of_document(self, list_of_document: list) -> None: pass
+    def insert_list_of_document(self, list_of_document: list) -> None:
+        pass
 
     @abstractmethod
-    def select_many(self, doc_filter: dict) -> list: pass 
+    def select_many(self, doc_filter: dict) -> list:
+        pass
 
     @abstractmethod
-    def select_one(self, doc_filter: dict) -> dict: pass
+    def select_one(self, doc_filter: dict) -> dict:
+        pass
 
     @abstractmethod
-    def select_many_with_properties(self, doc_filter: dict) -> list: pass
+    def select_many_with_properties(self, doc_filter: dict) -> list:
+        pass
 
     @abstractmethod
-    def select_if_property_exists(self) -> dict: pass
+    def select_if_property_exists(self) -> dict:
+        pass
 
     @abstractmethod
-    def select_by_object_id(self, object_id: str) -> dict: pass      
+    def select_by_object_id(self, object_id: str) -> dict:
+        pass
 
     @abstractmethod
-    def edit_registry(self) -> None: pass
+    def edit_registry(self, order_id: str, update_fields: dict) -> None:
+        pass
 
     @abstractmethod
-    def edit_many_registries(self) -> None: pass
+    def edit_many_registries(self) -> None:
+        pass
 
     @abstractmethod
-    def edit_registry_with_increment(self) -> None: pass      
+    def edit_registry_with_increment(self) -> None:
+        pass
 
     @abstractmethod
-    def delete_registry(self) -> None: pass
+    def delete_registry(self) -> None:
+        pass
 
     @abstractmethod
-    def delete_many_registries(self) -> None: pass
+    def delete_many_registries(self) -> None:
+        pass
         
