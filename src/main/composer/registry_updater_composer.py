8da8129e12bc00ae5edf0d77903971e417d99ae9
Maria Andreza C Santos <mandreza008@gmail.com>
2025-04-24 23:04:04 -0300

    feat: implementa composers

---
+++ src/main/composer/registry_updater_composer.py
@@ -0,0 +1,11 @@
+from src.models.repository.orders_repository import OrdersRepository
+from src.models.connection.connection_handler import db_connection_handler
+from src.use_cases.registry_updater import RegistryUpdater
+
+
+def registry_updater_composer():
+    conn = db_connection_handler.get_db_connection()
+    model = OrdersRepository(conn)
+    use_case = RegistryUpdater(model)
+
+    return use_case
